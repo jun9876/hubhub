7788f73340fd34f2134bb656d1df76ad2622ce9d
jok jors <junx0881@gmail.com>
2023-05-20 12:16:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page Dipan</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+         
+        }
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .logo {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        .logo img {
+            width: 200px;
+        }
+        .description {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        .cta-button {
+            text-align: center;
+        }
+        .cta-button a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #337ab7;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Selamat Datang di dunia furniture </h1>
+        <div class="logo">
+        <img src="img/Logo-dipan.png" alt="Logo Dipan">
+        </div>
+        <div class="description">
+            <p>Perusahaan Dipan adalah perusahaan yang bergerak di bidang produksi dan distribusi perabotan rumah tangga.</p>
+            <p>Kami menyediakan berbagai macam produk berkualitas dengan harga terjangkau.</p>
+        </div>
+        <div class="cta-button">
+            <a href="https://www.lazada.co.id/shop/tama-furniture">Lihat Produk Kami</a>
+        </div>
+    </div>
+</body>
+</html>
